99653ec0f1f29089cd6293e3b79091b1920622aa
Benjamin Bordne <benjibordne@outlook.com>
2025-03-24 18:11:50 +0100

    Updated Build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ tasks.register<JacocoReport>("jacocoTestReport") {
 
 sonar {
     properties {
-        property("sonar.projectKey", "AAU-SE2_WebSocketBrokerDemo-App")
-        property("sonar.organization", "aau-se2")
+        property("sonar.projectKey", "SE-Projekt-Beta_Client")
+        property("sonar.organization", "se-projekt-beta")
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.java.coveragePlugin", "jacoco")
         property(
