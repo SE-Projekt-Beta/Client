941a8de35cb32d008536d4dd5e198141a4f918a5
Benji <65447501+hibenji@users.noreply.github.com>
2025-03-24 17:58:30 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("org.sonarqube") version "6.0.1.5171"
-}
-
-sonar {
-  properties {
-    property("sonar.projectKey", "SE-Projekt-Beta_Client")
-    property("sonar.organization", "se-projekt-beta")
-    property("sonar.host.url", "https://sonarcloud.io")
-  }
 }
