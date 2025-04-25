520029f9eb436760570b66b4b04dd7367a775dd0
Thomas <thomasel@edu.aau.at>
2025-04-25 12:17:33 +0200

    refactor BoardMap and GameStateClientTest

---
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/game/GameStateClientTest.kt
@@ -0,0 +1,29 @@
+package at.aau.serg.websocketbrokerdemo.game
+
+import junit.framework.TestCase.assertEquals
+import org.junit.jupiter.api.Test
+
+class GameStateClientTest {
+
+    @Test
+    fun testUpdateAndGetPosition() {
+        GameStateClient.updatePosition("player1", 5)
+        val result = GameStateClient.getPosition("player1")
+        assertEquals(5, result)
+    }
+
+    @Test
+    fun testDefaultPositionForUnknownPlayer() {
+        val result = GameStateClient.getPosition("unknown")
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun testAllPositions() {
+        GameStateClient.updatePosition("playerA", 3)
+        GameStateClient.updatePosition("playerB", 7)
+        val all = GameStateClient.getAllPositions()
+        assertEquals(3, all["playerA"])
+        assertEquals(7, all["playerB"])
+    }
+}
