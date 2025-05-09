ff0cb4cd616d8c71bf0d897eb0dba8eb70b5d565
Thomas <thomasel@edu.aau.at>
2025-05-09 22:35:20 +0200

    add dialog boxes for risk card and bank card

---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/game/RiskCardDialog.kt
@@ -0,0 +1,27 @@
+package at.aau.serg.websocketbrokerdemo.game
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import at.aau.serg.websocketbrokerdemo.R
+
+class RiskCardDialog(
+    context: Context,
+    private val title: String,
+    private val description: String
+) : Dialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_risk_card)
+
+        findViewById<TextView>(R.id.riskCardTitle).text = title
+        findViewById<TextView>(R.id.riskCardText).text = description
+
+        findViewById<Button>(R.id.okRiskCardBtn).setOnClickListener {
+            dismiss()
+        }
+    }
+}
