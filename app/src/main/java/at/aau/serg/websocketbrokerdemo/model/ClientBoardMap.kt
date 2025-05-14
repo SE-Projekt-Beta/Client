ca06b9c1554c43ca3c671b2f35c56a308d87ac1b
Lena Simmerle <lesimmerle@edu.aau.at>
2025-05-14 09:05:36 +0200

    merged main into gameview

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/model/ClientBoardMap.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/model/ClientBoardMap.kt
@@ -4,6 +4,9 @@ import android.graphics.PointF
 
 object ClientBoardMap {
 
+    private const val RISIKO = "Risiko"
+    private const val BANK = "Bank"
+
     val tiles: List<ClientTile> = listOf(
         ClientTile(1, "Start", TileType.START, position = PointF(1937f, 1937f)),
         ClientTile(2, "Amtsplatz", TileType.STREET, 220, 80, 160, 320, position = PointF(1742f, 1937f)),
@@ -13,7 +16,7 @@ object ClientBoardMap {
         ClientTile(6, "Annenstraße", TileType.STREET, 250, 96, 150, 300, position = PointF(1070f, 1937f)),
         ClientTile(7, "Joanneumring", TileType.STREET, 220, 80, 130, 260, position = PointF(902f, 1937f)),
         ClientTile(8, "Eisenbahn Wien- Graz", TileType.STREET, 180, 56, 100, 200, position = PointF(734f, 1937f)),
-        ClientTile(9, "Bank", TileType.BANK, position = PointF(566f, 1937f)),
+        ClientTile(9, BANK, TileType.BANK, position = PointF(566f, 1937f)),
         ClientTile(10, "Joseph-Haydn-Gasse", TileType.STREET, 100, 24, 50, 100, position = PointF(398f, 1937f)),
         ClientTile(11, "Polizeikontrolle", TileType.GOTO_JAIL, position = PointF(231f, 1937f)),
         ClientTile(12, "Schlossgrund", TileType.STREET, 220, 80, 160, 320, position = PointF(111f, 1815f)),
@@ -27,12 +30,12 @@ object ClientBoardMap {
         ClientTile(20, "Stifterstraße", TileType.STREET, 180, 56, 100, 200, position = PointF(111f, 471f)),
         ClientTile(21, "Sondersteuer", TileType.TAX, position = PointF(111f, 303f)),
         ClientTile(22, "Museumstraße", TileType.STREET, 220, 80, 160, 320, position = PointF(231f, 111f)),
-        ClientTile(23, "Risiko", TileType.RISK, position = PointF(398f, 111f)),
+        ClientTile(23, RISIKO, TileType.RISK, position = PointF(398f, 111f)),
         ClientTile(24, "Autobuslinie", TileType.STREET, 210, 72, 120, 240, position = PointF(566f, 111f)),
         ClientTile(25, "Mirabellplatz", TileType.STREET, 250, 96, 150, 300, position = PointF(734f, 111f)),
         ClientTile(26, "Westbahnstraße", TileType.STREET, 240, 88, 140, 280, position = PointF(902f, 111f)),
         ClientTile(27, "Universitätsplatz", TileType.STREET, 250, 96, 150, 300, position = PointF(1070f, 111f)),
-        ClientTile(28, "Bank", TileType.BANK, position = PointF(1238f, 111f)),
+        ClientTile(28, BANK, TileType.BANK, position = PointF(1238f, 111f)),
         ClientTile(29, "Burggasse", TileType.STREET, 140, 40, 100, 200, position = PointF(1406f, 111f)),
         ClientTile(30, "Villacherstraße", TileType.STREET, 200, 64, 110, 220, position = PointF(1574f, 111f)),
         ClientTile(31, "Gefängnis", TileType.PRISON, position = PointF(1742f, 111f)),
@@ -42,7 +45,7 @@ object ClientBoardMap {
         ClientTile(35, "Maria-Theresien-Straße", TileType.STREET, 300, 120, 200, 400, position = PointF(1937f, 807f)),
         ClientTile(36, "Andreas-Hofer-Straße", TileType.STREET, 250, 96, 150, 300, position = PointF(1937f, 975f)),
         ClientTile(37, "Boznerplatz", TileType.STREET, 300, 120, 200, 400, position = PointF(1937f, 1143f)),
-        ClientTile(38, "Risiko", TileType.RISK, position = PointF(1937f, 1311f)),
+        ClientTile(38, RISIKO, TileType.RISK, position = PointF(1937f, 1311f)),
         ClientTile(39, "Arlbergstraße", TileType.STREET, 120, 32, 50, 100, position = PointF(1937f, 1479f)),
         ClientTile(40, "Rathausstraße", TileType.STREET, 180, 56, 100, 200, position = PointF(1937f, 1647f))
     )
